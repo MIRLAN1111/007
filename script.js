5a7a4c8d33e34d435cb15ad708f755a47498ca3f
MIRLAN1111 <_@gmail.com>
2026-02-19 15:47:39 +0600

    finish project amanchik

--- script.js
+++ script.js
@@ -6,25 +6,37 @@ class Student {
 		this.grades = [];
 	}
 	addGrade(grade) {
-		if (grade < 2 || grade > 5) {
+		if (grade < 2 || grade > 5 || isNaN(grade)) {
 			return "Ошибка: оценка должна быть от 2 до 5";
 		}
 		this.grades.push(grade);
 		return "Оценка добавлена";
 	}
-	getAvarge() {
+	getAverage() {
 		if (this.grades.length === 0) return 0;
 		let sum = this.grades.reduce((a, b) => a + b, 0);
 		return (sum / this.grades.length).toFixed(2);
 	}
 	getInfo() {
-		return `Студент: ${this.#name}, Средняя оценка: ${this.getAvarge()}`;
+		return `Студент: ${this.#name}, Средняя оценка: ${this.getAverage()}`;
+	}
+	getGrades() {
+		return this.grades.join(", ");
 	}
 }
-// значение бериш керек екен еле
-const student = new Student("студент");
-//
+
+const student = new Student("Amanbek ");
+
 function add() {
-	const grade = Number(document.getElementById("grade").value);
-	document.getElementById("info").textContent = student.addGrade(grade);
+	const gradeInput = document.getElementById("grade");
+	const grade = Number(gradeInput.value);
+	const message = student.addGrade(grade);
+
+	document.getElementById("info").textContent =
+		message + "\n" + student.getInfo();
+	document.getElementById("gradesList").textContent =
+		"Все оценки: " + student.getGrades();
+
+	gradeInput.value = "";
+	gradeInput.focus();
 }
